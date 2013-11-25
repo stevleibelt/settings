70cfdf3f998ffdce44f4ae2ba16bc5b1fa494360
stevleibelt <stev@leibelt.de>
2013-11-25 23:37:10 +0100

    Added todo

---
+++ i3/predefinedLayout.sh
@@ -0,0 +1,4 @@
+# http://infra.in.zekjur.net/archives/i3-discuss/2012-April/000626.html
+# https://faq.i3wm.org/question/1556/automatically-changing-the-layout-after-starting-the-second-program-on-a-workspace-similar-to-dwm/
+# https://github.com/fhaun/config-misc/blob/master/i3-stuff/auto-start-for-i3
+
