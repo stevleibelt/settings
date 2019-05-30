a17159ac0cd473f418d5d0921f7623a9ba4468db
stevleibelt <stev@leibelt.de>
2019-05-30 22:32:42 +0200

    updated

--- install.sh
+++ install.sh
@@ -54,22 +54,22 @@ read YES_OR_NO
 
 if [[ ${YES_OR_NO} != "n" ]];
 then
-    if [[ -d ~/.i3  ]]
+    if [[ -d ~/.config/i3  ]]
     then
-        echo "   Renaming ~/.i3 to ~/.i3.${DATETIME}"
-        mv ~/.i3 ~/.i3.${DATETIME}
+        echo "   Renaming ~/.config/i3 to ~/.config/i3.${DATETIME}"
+        mv ~/.config/i3 ~/.config/i3.${DATETIME}
     fi
 
-    echo "   Creating softlink for ~/.i3"
-    ln -s ${PATH_OF_THE_CURRENT_SCRIPT}/i3 ~/.i3
+    echo "   Creating softlink for ~/.config/i3"
+    ln -s ${PATH_OF_THE_CURRENT_SCRIPT}/i3 ~/.conifg/i3
 
-    if [[ -f .i3status.conf ]]
+    if [[ -f .config/i3status/config ]]
     then
-        echo "   Renaming ~/.i3status.conf to ~/.i3status.conf.${DATETIME}"
-        mv ~/.i3status.conf ~/.i3status.conf.${DATETIME}
+        echo "   Renaming ~/.config/i3status/config to ~/.config/i3status/config.${DATETIME}"
+        mv ~/.config/i3status/config ~/.config/i3status/config.${DATETIME}
 
         echo "   Creating new one from example configuration. You have to adapt it!"
-        cp ${PATH_OF_THE_CURRENT_SCRIPT}/i3/exampleStatus.conf ~/.i3status.conf
+        cp ${PATH_OF_THE_CURRENT_SCRIPT}/i3/exampleStatus.conf ~/.config/i3status/config
     fi
 fi
 #eo i3
