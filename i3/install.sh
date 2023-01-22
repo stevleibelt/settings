4e818c3761201de27f83847ef0dcc5676faad001
stevleibelt <artodeto@bazzline.net>
2023-01-22 23:17:23 +0100

    updated

--- i3/install.sh
+++ i3/install.sh
@@ -4,10 +4,13 @@
 function _main () {
   local PATH_TO_THIS_SCRIPT=$(cd $(dirname "${0}"); pwd)
 
-  /usr/bin/mkdir -p ~/.config/i3
-  cp "${PATH_TO_THIS_SCRIPT}/bre14_config" ~/.config/i3/config
+  if [[ ! -f "~/.config/i3/config" ]];
+  then
+    /usr/bin/mkdir -p ~/.config/i3
+    cp "${PATH_TO_THIS_SCRIPT}/bre14_config" ~/.config/i3/config
 
-  echo ":: Please adapt content of file >>~/.config/i3/config<<."
+    echo ":: Please adapt content of file >>~/.config/i3/config<<."
+  fi
 }
 
 _main ${@}
--- i3status/install.sh
+++ i3status/install.sh
@@ -4,10 +4,13 @@
 function _main () {
   local PATH_TO_THIS_SCRIPT=$(cd $(dirname "${0}"); pwd)
 
-  /usr/bin/mkdir -p ~/.config/i3status
-  cp "${PATH_TO_THIS_SCRIPT}/bre14_config" ~/.config/i3status/config
+  if [[ ! -f "~/.config/i3status/config" ]];
+  then
+    /usr/bin/mkdir -p ~/.config/i3status
+    cp "${PATH_TO_THIS_SCRIPT}/bre14_config" ~/.config/i3status/config
 
-  echo ":: Please adapt content of file >>~/.config/i3status/config<<."
+    echo ":: Please adapt content of file >>~/.config/i3status/config<<."
+  fi
 }
 
 _main ${@}
