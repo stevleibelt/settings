316bfa8a4c2c48b0d1ed4adc037f9d3fac81c9a0
stevleibelt <artodeto@bazzline.net>
2023-02-01 19:52:10 +0100

    addded xinit

--- i3/install.sh
+++ i3/install.sh
@@ -1,16 +1,34 @@
 #!/bin/bash
 ####
 
-function _main () {
+function _main ()
+{
   local PATH_TO_THIS_SCRIPT=$(cd $(dirname "${0}"); pwd)
 
-  if [[ ! -f "~/.config/i3/config" ]];
+  if [[ ! -f "${HOME}/.config/i3/config" ]];
   then
     /usr/bin/mkdir -p ~/.config/i3
-    cp "${PATH_TO_THIS_SCRIPT}/bre14_config" ~/.config/i3/config
+    cp "${PATH_TO_THIS_SCRIPT}/bre14_config" "${HOME}/.config/i3/config"
 
-    echo ":: Please adapt content of file >>~/.config/i3/config<<."
+    echo ":: Please adapt content of file >>${HOME}/.config/i3/config<<."
   fi
+
+  if [[ -f "${HOME}/.xinitrc" ]];
+  then
+    echo ":: File exists."
+    echo "   Moving >>${HOME}/.xinitrc<< to >>${HOME}/.xinitrc.tmp<<"
+    mv "${HOME}/.xinitrc" "${HOME}/.xinitrc.tmp"
+
+    cp "${PATH_TO_THIS_SCRIPT}/xinitrc" "${HOME}/.xinitrc"
+
+    echo "   You have to merge both files manually."
+    vimdiff "${HOME}/.xinitrc.tmp" "${HOME}/.xinitrc"
+
+    echo "   Please remove this file by hand >>${HOME}/.xinitrc.tmp<<"
+  else
+    cp "${PATH_TO_THIS_SCRIPT}/xinitrc" "${HOME}/.xinitrc"
+  fi
+
 }
 
 _main ${@}
