244f7fc06fdc684bbc2a7dd33ceac28b97b795ca
stevleibelt <stev.leibelt@hrz.tu-freiberg.de>
2023-06-12 11:59:06 +0200

    Add creation of directory btop

--- btop/install.sh
+++ btop/install.sh
@@ -1,2 +1,10 @@
+#!/bin/bash
+
 rm ~/.config/btop/btop.conf
+
+if [[ ! -d ~/.config/btop ]];
+then
+  mkdir -p ~/.config/btop
+fi
+
 ln -s ~/software/source/com/github/stevleibelt/settings/btop/btop.conf ~/.config/btop/
