05e1549cccd6f8e7017747137567437dad7fefdf
stevleibelt <stev.leibelt@hrz.tu-freiberg.de>
2023-06-12 13:30:38 +0200

    Add install.sh usage to readme

--- README.md
+++ README.md
@@ -4,25 +4,13 @@ This repository contains my settings and dot files.
 
 The current change log can be found [here](CHANGELOG.md).
 
-# Install
+## Install
+
+Just use the [install.sh](bin/install.sh)
 
 * symlink directories or files where needed
 
 ```
-# git
-ln -s <path>/git/gitconfig $HOME/.gitconfig
-ln -s <path>/git/gitignore.global $HOME/.gitignore.global
-
-# i3
-ln -s <path>/i3 $HOME/.config/i3
-# list available *.conf
-ln -s <path>/<conf>.conf $HOME/.config/i3status/config
-
-# abcde
-ln -s <path>/abcde/.abcde.conf $HOME/.abcde.conf
-
-# gnome-terminal-colors
-
 git submodule init
 git submodule update
 bash ./gnome-terminal-colors/install.sh
--- bin/install.sh
+++ bin/install.sh
@@ -8,13 +8,18 @@
 
 function _main ()
 {
-  local DATETIME
   local LIST_OF_INSTALL_SCRIPTS
   local PATH_OF_THE_CURRENT_SCRIPT
+  local PWD
 
   #begin of setup
-  DATETIME=$(date +%y%m%d_+%T)
   PATH_OF_THE_CURRENT_SCRIPT=$(cd $(dirname "${BASH_SOURCE[0]}"); pwd)
+  PWD=$(pwd)
+
+  cd "${PATH_OF_THE_CURRENT_SCRIPT}/../" || printf ":: Error\n   Could not change to directory >>%s<<" "${PATH_OF_CURRENT_INSTALL_SCRIPT}/../"
+  git submodule init
+  git submodule update
+  cd "${PWD}" || printf ":: Error\n   Could not change to directory >>%s<<." "${PWD}"
 
   #ref: https://stackoverflow.com/questions/23356779/how-can-i-store-the-find-command-results-as-an-array-in-bash/54561526#54561526
   #mapfile -d '' LIST_OF_INSTALL_SCRIPTS < <(find "${PATH_OF_THE_CURRENT_SCRIPT}/../" -name install.sh -type f -exec realpath {} \;)
@@ -22,7 +27,7 @@ function _main ()
 
   for PATH_OF_CURRENT_INSTALL_SCRIPT in "${LIST_OF_INSTALL_SCRIPTS[@]}";
   do
-    read -p ">  Do you want to execute ${PATH_OF_CURRENT_INSTALL_SCRIPT:${#PATH_OF_THE_CURRENT_SCRIPT}}? (Y|n) "
+    read -r -p ">  Do you want to execute ${PATH_OF_CURRENT_INSTALL_SCRIPT:${#PATH_OF_THE_CURRENT_SCRIPT}}? (Y|n) "
 
     if [[ ${REPLY:-y} =~ ^[Yy]$ ]];
     then
