7e9db6918ed52af1dcccdde124a99666e3312368
stevleibelt <stev.leibelt@hrz.tu-freiberg.de>
2024-02-29 10:18:28 +0100

    Add option to backup existing btop.conf

--- btop/install.sh
+++ btop/install.sh
@@ -1,10 +1,34 @@
 #!/bin/bash
+####
+# @since: 2023-04-15
+# @author: stev leibelt <artodeto@bazzline.net>
+####
 
-rm ~/.config/btop/btop.conf
+function _main() {
+  local CURRENT_SCRIPT_PATH
+  local SOURCE_FILE_NAME
+  local SOURCE_FILE_PATH
+  local TARGET_DIRECTORY_PATH
+  local TARGET_FILE_PATH
 
-if [[ ! -d ~/.config/btop ]];
-then
-  mkdir -p ~/.config/btop
-fi
+  CURRENT_SCRIPT_PATH=$(dirname $(realpath "${0}"))
+  SOURCE_FILE_NAME="${1}"
+  TARGET_DIRECTORY_PATH="${2}"
 
-ln -s ~/software/source/com/github/stevleibelt/settings/btop/btop.conf ~/.config/btop/
+  SOURCE_FILE_PATH="${CURRENT_SCRIPT_PATH}/${SOURCE_FILE_NAME}"
+  TARGET_FILE_PATH="${TARGET_DIRECTORY_PATH}/${SOURCE_FILE_NAME}"
+
+  if [[ -f "${TARGET_FILE_PATH}" ]];
+  then
+    mv "${TARGET_FILE_PATH}" "${TARGET_FILE_PATH}.bak"
+  fi
+
+  if [[ ! -d "${TARGET_DIRECTORY_PATH}" ]];
+  then
+    mkdir -p "${TARGET_DIRECTORY_PATH}"
+  fi
+
+  ln -s "${SOURCE_FILE_PATH}" "${TARGET_FILE_PATH}"
+}
+
+_main "btom.conf" "${HOME}/.config/btop"
