681ac3ff8db00db76767e1e6d37fb7f1e0ed5e71
stevleibelt <stev.leibelt@hrz.tu-freiberg.de>
2024-02-29 10:13:20 +0100

    Add bottom

---
+++ bottom/install.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+####
+# @since: 2024-02-29
+# @author: stev leibelt <artodeto@bazzline.net>
+####
+
+function _main() {
+  local CURRENT_SCRIPT_PATH
+  local SOURCE_FILE_NAME
+  local SOURCE_FILE_PATH
+  local TARGET_DIRECTORY_PATH
+  local TARGET_FILE_PATH
+
+  CURRENT_SCRIPT_PATH=$(dirname $(realpath "${0}"))
+  SOURCE_FILE_NAME="${1}"
+  TARGET_DIRECTORY_PATH="${2}"
+
+  SOURCE_FILE_PATH="${CURRENT_SCRIPT_PATH}/${SOURCE_FILE_NAME}"
+  TARGET_FILE_PATH="${TARGET_DIRECTORY_PATH}/${SOURCE_FILE_NAME}"
+
+  if [[ -f "${TARGET_FILE_PATH}" ]];
+  then
+    mv "${TARGET_FILE_PATH}" "${TARGET_FILE_PATH}.bak"
+  fi
+
+  if [[ ! -d "${TARGET_DIRECTORY_PATH}" ]];
+  then
+    mkdir -p "${TARGET_DIRECTORY_PATH}"
+  fi
+
+  ln -s "${SOURCE_FILE_PATH}" "${TARGET_FILE_PATH}"
+}
+
+_main "bottom.toml" "${HOME}/.config/bottom"
